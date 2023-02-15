77159e5c81766ca17962d001980878154b6cdef3
Lester Batres <lester@soyfri.com>
2023-02-16 00:10:42 +0100

    SDK bancos.

---
+++ Fri_SDK.xcframework/ios-arm64_x86_64-simulator/Fri_SDK.framework/Headers/Fri_SDK.h
@@ -0,0 +1,18 @@
+//
+//  Fri_SDK.h
+//  Fri SDK
+//
+//  Created by Lester Batres on 20.11.22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Fri_SDK.
+FOUNDATION_EXPORT double Fri_SDKVersionNumber;
+
+//! Project version string for Fri_SDK.
+FOUNDATION_EXPORT const unsigned char Fri_SDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Fri_SDK/PublicHeader.h>
+
+
